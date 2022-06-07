0601ebf5e5412291b0b3b6a4973d86081eea1823
dujunnan <ready.now@foxmail.com>
2022-06-07 15:11:22 +0800

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by 杜俊楠 on 2022/6/7.
+//  Copyright © 2022 杜俊楠. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
